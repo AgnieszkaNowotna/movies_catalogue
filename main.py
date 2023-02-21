96d9dcc5ace808aaa87d91af2b4c9cd26d355313
Agnieszka Nowotna <a.ilkow7@gmail.com>
2023-02-21 18:59:03 +0100

    Add detail page

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_random_movies(8)
-    return render_template("homepage.html", movies = movies)
+    movie_types = ['now_playing','popular','top_rated','upcoming']
+    selected_list = tmdb_client.get_list_type(movie_types)
+    movies = tmdb_client.get_random_movies(how_many = 8, list_name = selected_list)
+    return render_template("homepage.html", movies = movies, movie_types = movie_types, selected_list = selected_list)
+
+@app.route('/movie/<int:movie_id>')
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    image = tmdb_client.get_random_movie_image(movie_id)
+    print(type(details))
+    return render_template("movie_details.html", movie = details, cast=cast, image=image)
 
 @app.context_processor
 def utility_processor():
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,12 +1,37 @@
-from random import randrange
+from flask import request
+from random import randrange, choice
 import requests
-
+API_TOKEN = 'api token'
 
 def get_popular_movies():
     endpoint = 'https://api.themoviedb.org/3/movie/popular'
-    api_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjY5NzQzNDdmOTFlNzQ2NWQ5YjViM2M3ZDVkM2ExMSIsInN1YiI6IjYzZjM0ZjA0YTI0YzUwMDA4MDBjYzYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QSwOJ5NE30GFESKOCx00kGxEHl5DFJ5EruMWHiAb65k'
-    headers = {'Authorization': f'Bearer {api_token}'}
+    headers = {'Authorization': f'Bearer {API_TOKEN}'}
     response = requests.get (endpoint, headers = headers)
+    return response.json()
+
+def get_single_movie(movie_id):
+    endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}'
+    headers = {'Authorization': f'Bearer {API_TOKEN}'}
+    response = requests.get (endpoint, headers = headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f'https://api.themoviedb.org/3/movie/{movie_id}/credits'
+    headers = {'Authorization': f'Bearer {API_TOKEN}'}
+    response = requests.get (endpoint, headers = headers)
+    return response.json()["cast"]
+
+def get_single_movie_images(movie_id):
+    endpoint = f'https://api.themoviedb.org/3//movie/{movie_id}/images'
+    headers = {'Authorization': f'Bearer {API_TOKEN}'}
+    response = requests.get (endpoint, headers = headers )
+    return response.json()["backdrops"]
+
+def get_movies_list(list_name = 'popular'):
+    endpoint = f'https://api.themoviedb.org/3/movie/{list_name}'
+    headers = {'Authorization': f'Bearer {API_TOKEN}'}
+    response = requests.get (endpoint, headers = headers)
+    response.raise_for_status()
     return response.json()
 
 def get_poster_url(poster_api_path, size = 'w342'):
@@ -14,15 +39,25 @@ def get_poster_url(poster_api_path, size = 'w342'):
     url = f'{base_url}{size}/{poster_api_path}'
     return url
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_name = 'popular'):
+    data = get_movies_list(list_name)
     return data["results"][:how_many]
 
-def get_random_movies(how_many):
+def get_random_movies(how_many, list_name = 'popular'):
     data = []
-    movies = get_popular_movies()['results']
+    movies = get_movies_list(list_name)['results']
     for i in range (how_many):
         position = randrange(len(movies))
         data.append(movies[position])
         movies.remove(movies[position])
     return data
+
+def get_random_movie_image(movie_id):
+    image = choice(get_single_movie_images(movie_id))
+    return image
+
+def get_list_type(movie_types):
+    for movie_type in movie_types:
+        if request.args.get('list_type') == movie_type:
+            return request.args.get('list_type', 'popular')
+    return 'popular'
